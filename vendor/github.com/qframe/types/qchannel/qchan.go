c5e4e47d1e4815d6d73bae0415198ef74c87459e
Christian Kniep <christian@qnib.org>
2017-09-18 10:41:22 +0200

    incorperate engineInfo

--- vendor/github.com/qframe/types/qchannel/qchan.go
+++ vendor/github.com/qframe/types/qchannel/qchan.go
@@ -1,30 +1,30 @@
-package qtypes
+package qtypes_qchannel
+
 
 import (
-	"os"
 	"log"
 
 	"github.com/grafov/bcast"
 	"github.com/zpatrick/go-config"
 	"fmt"
 	"strings"
+	"github.com/qframe/types/helper"
 )
 
 // QChan holds the broadcast channels to communicate
 type QChan struct {
 	Data *bcast.Group
-	Back *bcast.Group
+	Done *bcast.Group
 	Tick *bcast.Group
 	Cfg *config.Config
-	Done chan os.Signal
 }
 
 func (qc *QChan) Log(logLevel, msg string) {
 	// TODO: Setup in each Log() invocation seems rude
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 	dL, _ := qc.Cfg.StringOr("log.level", "info")
-	dI := LogStrToInt(dL)
-	lI := LogStrToInt(logLevel)
+	dI := qtypes_helper.LogStrToInt(dL)
+	lI := qtypes_helper.LogStrToInt(logLevel)
 	lMsg := fmt.Sprintf("[%+6s] %s", strings.ToUpper(logLevel), msg)
 	if lI == 0 {
 		log.Panic(lMsg)
@@ -44,18 +44,17 @@ func NewQChan() QChan {
 func NewCfgQChan(cfg *config.Config) QChan {
 	return QChan{
 		Data: bcast.NewGroup(), // create broadcast group
-		Back: bcast.NewGroup(), // create broadcast group
+		Done: bcast.NewGroup(), // create broadcast group
 		Tick: bcast.NewGroup(), // create broadcast group
 		Cfg: cfg,
-		Done: make(chan os.Signal, 1),
 	}
 }
 
 
 func (qc *QChan) Broadcast() {
-	qc.Log("info", "Dispatch broadcast for Back, Data and Tick")
+	qc.Log("info", "Dispatch broadcast for Data, Done and Tick")
 	go qc.Data.Broadcast(0)
-	go qc.Back.Broadcast(0)
+	go qc.Done.Broadcast(0)
 	go qc.Tick.Broadcast(0)
 }
 
